ccd0b15a8a4e521fa73100f4d8bc2aadd9fac0ab
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-01-17 23:56:08 +0200

    Implement DiffUtil in TodoListAdapter

--- app/src/main/java/com/seif/todoit/ui/adapters/TodoListAdapter.kt
+++ app/src/main/java/com/seif/todoit/ui/adapters/TodoListAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.seif.todoit.R
 import com.seif.todoit.data.models.PriorityModel
@@ -17,16 +18,16 @@ class TodoListAdapter() : RecyclerView.Adapter<TodoListAdapter.MyViewHolder>() {
 
     class MyViewHolder(val binding: TodoRowDesignBinding) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(position: Int, todoList: List<TodoModel>){
-                binding.todoTitleTxt.text = todoList[position].title
-                binding.todoDescriptionTxt.text = todoList[position].description
+        fun bind(position: Int, todoList: List<TodoModel>) {
+            binding.todoTitleTxt.text = todoList[position].title
+            binding.todoDescriptionTxt.text = todoList[position].description
 
-                binding.todoItemCons.setOnClickListener {
-                    val action =
-                        ToDoListFragmentDirections.actionToDoListFragmentToUpdateTodoFragment(todoList[position])
-                    itemView.findNavController().navigate(action)
-                }
+            binding.todoItemCons.setOnClickListener {
+                val action =
+                    ToDoListFragmentDirections.actionToDoListFragmentToUpdateTodoFragment(todoList[position])
+                itemView.findNavController().navigate(action)
             }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -38,7 +39,7 @@ class TodoListAdapter() : RecyclerView.Adapter<TodoListAdapter.MyViewHolder>() {
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.bind(position, todoList)
-        setSpinnerPriorityColor(holder,position)
+        setSpinnerPriorityColor(holder, position)
 
     }
 
@@ -46,12 +47,14 @@ class TodoListAdapter() : RecyclerView.Adapter<TodoListAdapter.MyViewHolder>() {
         return todoList.size
     }
 
-    fun setData(todoList2: List<TodoModel>) {
-        this.todoList = todoList2
-        notifyDataSetChanged()
+    fun setData(newTodoList: List<TodoModel>) {
+        val diffUtilCallback = TodoDiffUtil(todoList, newTodoList)
+        val diffUtilResult = DiffUtil.calculateDiff(diffUtilCallback)
+        this.todoList = newTodoList
+        diffUtilResult.dispatchUpdatesTo(this)
     }
 
-    private fun setSpinnerPriorityColor(holder: MyViewHolder, position: Int){
+    private fun setSpinnerPriorityColor(holder: MyViewHolder, position: Int) {
         when (todoList[position].priority) {
             PriorityModel.HIGH -> holder.binding.priorityIndicator.setCardBackgroundColor(
                 ContextCompat.getColor(
@@ -74,3 +77,10 @@ class TodoListAdapter() : RecyclerView.Adapter<TodoListAdapter.MyViewHolder>() {
         }
     }
 }
+/**
+ *  notifyDataSetChanged()
+ *  it's problem that it's assume that all existing items and structure may no longer be valid.
+ *  LayoutManagers will be forced to fully rebind and relayout all visible views.
+ *  if you are writing an adapter it will always be more efficient to use the more specific
+ *  change events if you can. Rely on notifyDataSetChanged() as a last resort.
+ * */
